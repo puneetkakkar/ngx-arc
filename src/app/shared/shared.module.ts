bef45527214ee8ed6172e88d693dce3374904d90
Puneet Kakkar <puneetkakkar91@gmail.com>
2020-01-11 13:16:38 +0530

    remove shared module sandbox

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
-import { SharedSandbox } from './shared.sandbox';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, FlexLayoutModule, RouterModule],
   exports: [FlexLayoutModule],
-  providers: [SharedSandbox],
+  providers: [],
 })
 export class SharedModule {}
