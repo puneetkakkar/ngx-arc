4290dcf632a4b404776aacee94b17fc605cd2159
Puneet Kakkar <puneetkakkar91@gmail.com>
2020-02-11 01:43:15 +0530

    @nguniversal/express-engine migration - update-9 Update @nguniversal/express-engine to version 9.

--- server.ts
+++ server.ts
@@ -1,85 +1,60 @@
-/**
- * *** NOTE ON IMPORTING FROM ANGULAR AND NGUNIVERSAL IN THIS FILE ***
- *
- * If your application uses third-party dependencies, you'll need to
- * either use Webpack or the Angular CLI's `bundleDependencies` feature
- * in order to adequately package them for use on the server without a
- * node_modules directory.
- *
- * However, due to the nature of the CLI's `bundleDependencies`, importing
- * Angular in this file will create a different instance of Angular than
- * the version in the compiled application code. This leads to unavoidable
- * conflicts. Therefore, please do not explicitly import from @angular or
- * @nguniversal in this file. You can export any needed resources
- * from your application's main.server.ts file, as seen below with the
- * import for `ngExpressEngine`.
- */
-
-import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
-import { createWindow } from 'domino';
-import * as express from 'express';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 import 'zone.js/dist/zone-node';
 
-const templateA = readFileSync(join('dist/browser', 'index.html')).toString();
-const win = createWindow(templateA);
-win['Object'] = Object;
-win['Math'] = Math;
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import * as express from 'express';
+import { join } from 'path';
 
-global['window'] = win;
-global['document'] = win.document;
-global['branch'] = null;
-global['object'] = win['object'];
-global['HTMLElement'] = win['HTMLElement'];
-global['navigator'] = win.navigator;
+import { AppServerModule } from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
 
-// Express server
-const app = express();
+// The Express app is exported so that it can be used by serverless Functions.
+export function app() {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
-const PORT = process.env.PORT || 4001;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {
-  AppServerModuleNgFactory,
-  LAZY_MODULE_MAP,
-  ngExpressEngine,
-  provideModuleMap,
-} = require('./dist/server/main');
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', (_: any, options: any, callback: any) => {
-  const engine = ngExpressEngine({
-    bootstrap: AppServerModuleNgFactory,
-    providers: [
-      { provide: REQUEST, useFactory: () => options.req, deps: [] },
-      { provide: RESPONSE, useFactory: () => options.req.res, deps: [] },
-      provideModuleMap(LAZY_MODULE_MAP),
-    ],
+  // Example Express Rest API endpoints
+  // app.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+  }));
+
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
   });
-  engine(_, options, callback);
-});
 
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+  return server;
+}
 
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
-// Serve static files from /browser
-app.get(
-  '*.*',
-  express.static(DIST_FOLDER, {
-    maxAge: '1y',
-  }),
-);
+function run() {
+  const port = process.env.PORT || 4000;
 
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
 
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export * from './src/main.server';
